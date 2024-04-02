5282f0afe469b1f69a3c76663bea8f0a12e28548
MelissaH <iheartumedicdroid@gmail.com>
2024-04-02 15:16:48 -0700

    feat: add BankAcct, SavingsAcct, StudentComparator

--- Inheritance.java
+++ Inheritance.java
@@ -1,51 +1,32 @@
+import java.util.ArrayList;
+
 /**
  * Inheritance
  */
 public class Inheritance {
 
     public static void main(String[] args) {
+        BankAccount account = new BankAccount(3.0);
+        account.withdraw(10);
+        System.out.println(account.getBalance());
+        SavingsAccount savingsAcct = new SavingsAccount(2.1);
+        savingsAcct.deposit(500);
+        savingsAcct.withdraw(10);
+        System.out.println(savingsAcct.getBalance());
 
-        int[] arr = new int[3];
 
-        try{
-        System.out.println("Hello this is before the exception");
-        int value = arr[-1];
-        System.out.println("Is this going to display with an error");
-        } catch (Exception exception) {
-            System.out.println("Exception: " + exception);
-        } finally {
-            System.out.println("Hit the finally");
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Paul"));
+        students.add(new Student("Peter"));
+        students.add(new Student("Parker"));
+
+        students.sort(new StudentComparator());
+
+        for (Student student : students) {
+            System.out.println(student.GetName());
         }
-        
-        System.out.println("This is after the exception");
-
-        Student student = new Student("Melissa");
-        student.gpa = 3.0f;
-        System.out.println(student.GetName());
-
-        Instructor instructor = new Instructor("Donald");
-        instructor.salary = 50000;
-        System.out.println(instructor.GetName());
-
-        // User user = new User("Justin");
-        // System.out.println(user);
-
-        System.out.println(student instanceof Student);
-        System.out.println(student instanceof User);
-        //System.out.println(instructor instanceof Student);
-        System.out.println(instructor instanceof User);
-        // System.out.println(user instanceof Student);
 
 
-        Triangle triangle = new Triangle(5, 10);
-        System.out.println(triangle.GetArea());
-
-
-        Rectangle rectangle = new Rectangle(5, 10);
-        System.out.println(rectangle.GetArea());
-
-        Circle circle = new Circle(5);
-        System.out.println(circle.GetArea());
     }
     
 
